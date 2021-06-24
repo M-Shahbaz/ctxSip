9efb8b799fc7598b3a71ec779b946ca6f74d515d
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2021-06-24 17:08:29 +0500

    Update test.php

--- test.php
+++ test.php
@@ -4,6 +4,14 @@ $gitDir = __DIR__.('/../git/ctxSip/');
 $gitFile = $gitDir.'packed-refs';
 if (file_exists($gitFile)) {
 	echo 'The file ' . $gitFile . ' exists';
+	$file = fopen($gitFile, "r");
+	while(!feof($file)) {
+	  $line     = fgets($file); 
+	  $lineAry = explode(" refs/heads/master", $line);
+	  if(isset($lineAry[1]) && strlen($lineAry[0])>=3){
+		  var_dump($lineAry[0]);
+	  }   
+	}
 } else {
 	echo 'The file ' . $gitFile . ' does not exist';
 }
