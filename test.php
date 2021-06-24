140f5e2320937adf22ea5320c3439d1da1b510a1
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2021-06-24 16:40:16 +0500

    ...

--- test.php
+++ test.php
@@ -1,1 +1,11 @@
-786/92;
+<?php
+$gitDir = __DIR__.('/../git/ctxSip/');
+
+$gitFile = $gitDir.'packed-refs';
+if (file_exists($gitFile)) {
+	echo 'The file ' . $gitFile . ' exists';
+} else {
+	echo 'The file ' . $gitFile . ' does not exist';
+}
+
+?>
